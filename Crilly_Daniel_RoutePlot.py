85b7fced7ed770b37d33ad0cefc0be51a66b90f9
CrillyD <110904567+CrillyD@users.noreply.github.com>
2022-08-10 20:34:08 +0100

    Update Crilly_Daniel_RoutePlot.py

--- Crilly_Daniel_RoutePlot.py
+++ Crilly_Daniel_RoutePlot.py
@@ -2,12 +2,13 @@
 def check_file(file_name, files):
     checking = True
     while checking:
-        if file_name in files:
+        if file_name in files or file_name.lower() == "off":
             return file_name
             checking = False
+
             break
         else:
-            file_name = input("File not found, type in route again")
+            file_name = input("File not found, type in route again. Or type off to close")
 
 
 list_of_files = ["route001.txt", "route002.txt", "route003.txt"]
@@ -15,11 +16,13 @@ on = True
 
 while on:
     # If file passes check, save as list
-    file = check_file(input("What route would you like to take?"), list_of_files)
-    with open(file) as route_file:
-        route_text = route_file.readlines()
-
-
+    file = check_file(input("What route would you like to take? (Type off to quit)"), list_of_files)
+    if file == "off":
+        on = False
+        break
+    else:
+        with open(file) as route_file:
+            route_text = route_file.readlines()
 
     # clean the list of all \n's so I'm left with the co-ordinates and directions
     clean_route_text = []
@@ -44,33 +47,40 @@ while on:
         else:
             list_of_coordinates.append([list_of_coordinates[-1][0] - 1, list_of_coordinates[-1][-1]])
 
-    # check to see if route is inbounds
-    for i in list_of_coordinates:
-        if i[0] == 0 or i[0] == 13 or i[1] == 0 or i[1] == 13:
-            print("Error: The route is outside of the grid.")
-            on = False
-            break
-    if not on:
-        break
-    # create route
+        # check to see if route is inbounds
+        for i in list_of_coordinates:
+            if i[0] == 0 or i[0] == 13 or i[1] == 0 or i[1] == 13:
+                print("Error: The route is outside of the grid.")
+                on = False
+            if not on:
+                break
 
-    blank_grid = [["12", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
-                  ["11", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
-                  ["10", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
-                  ["9", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
-                  ["8", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
-                  ["7", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
-                  ["6", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
-                  ["5", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
-                  ["4", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
-                  ["3", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
-                  ["2", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
-                  ["1", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
-                  ["+", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]]
-    for i in list_of_coordinates:
-        blank_grid[12 - i[1]][i[0]] = "X"
+    if on:
+        # create route
 
-    for i in blank_grid:
-        print(i)
-    for i in list_of_coordinates:
-        print(i)
+        blank_grid = [["12", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
+                      ["11", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
+                      ["10", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
+                      ["9", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
+                      ["8", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
+                      ["7", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
+                      ["6", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
+                      ["5", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
+                      ["4", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
+                      ["3", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
+                      ["2", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
+                      ["1", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
+                      ["+", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]]
+        for i in list_of_coordinates:
+            blank_grid[12 - i[1]][i[0]] = "X"
+
+        for i in blank_grid:
+            print(i)
+
+        for i in list_of_coordinates:
+            print(i)
+        # Ask if we want to carry on
+    else:
+        carry_on = input("Would you like to continue?").lower()
+        if carry_on == "yes":
+            on = True
